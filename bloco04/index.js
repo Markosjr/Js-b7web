b50df5bc931e0c96cad88394d5ba3892f88f6059
Marcos Junior <markosjr1969@gmail.com>
2021-12-16 16:36:59 -0300

    numbers

---
+++ bloco04/index.js
@@ -0,0 +1,11 @@
+let num1 = 10.45634267;
+let num2 = 2.5;
+
+// console.log(num1.toString() + num2); Concatenação de num1 com num2
+// num1 = num1.toString();  Conversão de number para string
+// console.log(num1.toString(2)); Mostrando o valor da variável num1 em binário
+// console.log(typeof num1);  Mostrando o tipo da variável num1 
+
+// console.log(num1.toFixed(2)); Arrendodamento de 2 casas decimais 
+
+console.log(Number.isReal(num1))
