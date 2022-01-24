ecc4d9ad9b2f8357d805c66bc21fd575ac06d5d4
Marcos Junior <markosjr1969@gmail.com>
2022-01-24 17:28:57 -0300

    variaveis com string

--- b7web/index.js
+++ b7web/index.js
@@ -6,11 +6,22 @@
 
 // console.log("Essa mensagem irá aparecer no console."); // Geralmente para desenvolvedores
 
+// =============================================================
 
-var x = 10;
-var y = 15;
 
-var total = x + y;
+// var x = 10;
+// var y = 15;
 
-alert (total);
+// var total = x + y;
 
+// alert (total);
+
+// =============================================================
+
+
+var nome = "Marcos";
+var sobrenome = "Junior";
+
+var nomecompleto = nome + sobrenome;
+
+alert(nomecompleto);
