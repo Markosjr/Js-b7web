c4f63ee9870a129becf0fcc8d4c74207666239d0
Marcos Junior <markosjr1969@gmail.com>
2022-01-24 17:13:15 -0300

    4 formas de mostrar mensagens no navegador

---
+++ b7web/index.js
@@ -0,0 +1,7 @@
+document.getElementById("titulo").innerHTML = "Alteração no título"; // altera o documento 
+
+alert("Opa,Tudo bem?");
+
+document.write("Hello, World"); // escreve na janela do navegador
+
+console.log("Essa mensagem irá aparecer no console."); // Geralmente para desenvolvedores
