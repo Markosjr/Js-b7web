92c8befc2e07684809117711cd3517dab47003f8
Marcos Junior <markosjr1969@gmail.com>
2022-01-24 21:42:16 -0300

    utilizacao do ||

--- b7web/index.js
+++ b7web/index.js
@@ -26,13 +26,26 @@
 
 // alert(nomecompleto);
 
-var hora = window.prompt();
+// =====================================================================
 
-if ( hora < 12) {
-    console.log("Bom dia, já é ", hora, "horas");
-} else if (hora >= 12 && hora < 19 ) {
-    console.log("Boa tarde, já é ", hora, "horas")
-} else if (hora > 19) {
-    console.log("Boa noite, já é ", hora, "horas");
+// var hora = window.prompt();
 
-}
+// if ( hora < 12) {
+//     console.log("Bom dia, já é ", hora, "horas");
+// } else if (hora >= 12 && hora < 19 ) {
+//     console.log("Boa tarde, já é ", hora, "horas")
+// } else if (hora <=23 ) {
+//     console.log("Boa noite, já é ", hora, "horas");
+
+// }
+
+// =====================================================================
+
+
+// var hora = window.prompt();
+
+// if (hora == 12 || hora == 18) { // "||" SIGNIFICA OU 
+//     console.log("Você está na hora do RUSH");
+// }
+
+// ===========================================
